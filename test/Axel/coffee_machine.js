efb2d594b825f40e62c8487933b44113d2a35fd3
Axel Dujardin <axel.dujardin@Digital-Grenoble.local>
2017-12-11 16:51:29 +0100

    update file

---
+++ test/Axel/coffee_machine.js
@@ -0,0 +1,25 @@
+
+//console.log('Quelques ressources : ');
+//console.log(' - https://www.w3schools.com/css/css_background.asp');
+
+$(document).ready(function(){
+  function selectDrink (doSelect, drink) {
+    let doSelect
+    let drink
+  };
+  $('#btn-b').click(function(){
+    console.log("button clicked look next")
+    if (this === ".led-off.led-drink") {
+      $(this).removeClass("led-off")
+      $(this).addClass("led-on")
+      console.log("click work");
+  };
+
+  });
+});
+
+
+// $('.btn-ad').click(function(){
+//
+//   console.log("");
+//});
--- test/Axel/script.js
+++ test/Axel/script.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
+  $('.btn-ad').click(function(){
 
+    console.log("");
+  });
 });
