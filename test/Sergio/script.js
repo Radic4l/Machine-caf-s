8d35a209b7474f6c38de2c312ae5433a13577e85
Sergio Custodio <sergio.custodio@Digital-Grenoble.local>
2017-12-12 13:44:19 +0100

    function addDrink pour le the

--- test/Sergio/script.js
+++ test/Sergio/script.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
+	function selectDrink(doSelect, drink){
+		if (doSelect === true && drink === 'the'){
+			$('.btnThe').attr('src','../../asset/img/Vue1/Tea_click.png')
+		}
+		else{
+			(doSelect === false)
+			$('.btnThe').attr('src','../../asset/img/Vue1/Choixboisson_tea.png')
+		}
+	}
+
+	$('.btnThe').click(function(){
+		selectDrink(true, 'the')
+	})
+	$('.btnThe').dblclick(function(){
+		selectDrink(false, 'the')
+	});
+
 
 });
